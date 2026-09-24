11e3f656ee7c87ef4bfb3d92970cf9f3de5a8d69
Max Narongchai <Max.Narongchai12@gmail.com>
2026-09-24 14:35:23 +0100

    Adds initial script

---
+++ clean.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+
+INPUT_CSV = "nimbus_orders_messy.csv"
+OUTPUT_CSV = "nimbus_orders_clean.csv"
+
+def clean_orders(input_csv, output_csv):
+    df = pd.read_csv(input_csv)
+
+    print(f"Before dropping junk rows: {len(df)}")
+    
+    df.drop_duplicates(inplace=True)
+    df.dropna(inplace=True)
+
+    print(f"After dropping junk rows: {len(df)}")
+
+    df.to_csv(output_csv)
+    
+
+
+clean_orders(INPUT_CSV, OUTPUT_CSV)
