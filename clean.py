a118028c324214c9d1b277644a043144e9abe5d4
Patrick Quilty <pquilty04@gmail.com>
2026-09-24 14:49:33 +0100

    questions 8 and 9

--- clean.py
+++ clean.py
@@ -15,7 +15,22 @@ def clean_orders(input_csv, output_csv):
     print(f"After dropping junk rows: {len(df)}")
 
     df.to_csv(output_csv)
-    
 
+def unique_more_than_one(input_csv):
+    df = pd.read_csv(input_csv)
+
+
+def seattle_revenue(input_csv):
+    df = pd.read_csv(input_csv)
+    total = df.loc[df["city"] == "Seattle", "total"].sum()
+    print(total)
+
+def outdoor_total(input_csv):
+    df = pd.read_csv(input_csv)
+    total = df.loc[df["category"] == "Outdoor", "quantity"].sum()
+    print(total)
 
 clean_orders(INPUT_CSV, OUTPUT_CSV)
+seattle_revenue(OUTPUT_CSV)
+outdoor_total(OUTPUT_CSV)
+
